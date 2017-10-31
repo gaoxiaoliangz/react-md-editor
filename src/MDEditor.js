a6c9c3dd76124aef82dc25eac6664fc5f7898a13
Liang <gaoxiaoliangz@users.noreply.github.com>
2017-10-31 18:06:28 +0800

    mod

--- src/MDEditor.js
+++ src/MDEditor.js
@@ -10,30 +10,35 @@ require('codemirror/addon/edit/continuelist');
 
 import { getCursorState, applyFormat } from './format.js';
 
-var MarkdownEditor = React.createClass({
+class MarkdownEditor extends React.Component {
 
-	propTypes: {
-		onChange: React.PropTypes.func,
-		options: React.PropTypes.object,
-		path: React.PropTypes.string,
-		value: React.PropTypes.string,
-	},
-
-	getInitialState () {
+	static propTypes() {
 		return {
+			onChange: React.PropTypes.func,
+			options: React.PropTypes.object,
+			path: React.PropTypes.string,
+			value: React.PropTypes.string,
+		}
+	}
+
+	constructor(props) {
+		super(props)
+		this.state = {
 			isFocused: false,
 			cs: {},
-		};
-	},
+		}
+	}
 
 	componentDidMount () {
-		this.codeMirror = CM.fromTextArea(ReactDOM.findDOMNode(this.refs.codemirror), this.getOptions());
-		this.codeMirror.on('change', this.codemirrorValueChanged);
+		var $textarea = ReactDOM.findDOMNode(this.codemirror)
+		console.log($textarea)
+		this.codeMirror = CM.fromTextArea($textarea, this.getOptions());
+		this.codeMirror.on('change', this.codemirrorValueChanged.bind(this));
 		this.codeMirror.on('focus', this.focusChanged.bind(this, true));
 		this.codeMirror.on('blur', this.focusChanged.bind(this, false));
-		this.codeMirror.on('cursorActivity', this.updateCursorState);
+		this.codeMirror.on('cursorActivity', this.updateCursorState.bind(this));
 		this._currentCodemirrorValue = this.props.value;
-	},
+	}
 
 	getOptions () {
 		return Object.assign({
@@ -42,53 +47,53 @@ var MarkdownEditor = React.createClass({
 			indentWithTabs: true,
 			tabSize: '2',
 		}, this.props.options);
-	},
+	}
 
 	componentWillUnmount () {
 		// todo: is there a lighter-weight way to remove the cm instance?
 		if (this.codeMirror) {
 			this.codeMirror.toTextArea();
 		}
-	},
+	}
 
 	componentWillReceiveProps (nextProps) {
 		if (this.codeMirror && this._currentCodemirrorValue !== nextProps.value) {
 			this.codeMirror.setValue(nextProps.value);
 		}
-	},
+	}
 
 	getCodeMirror () {
 		return this.codeMirror;
-	},
+	}
 
 	focus () {
 		if (this.codeMirror) {
 			this.codeMirror.focus();
 		}
-	},
+	}
 
 	focusChanged (focused) {
 		this.setState({ isFocused: focused });
-	},
+	}
 
 	updateCursorState () {
 		this.setState({ cs: getCursorState(this.codeMirror) });
-	},
+	}
 
 	codemirrorValueChanged (doc, change) {
 		var newValue = doc.getValue();
 		this._currentCodemirrorValue = newValue;
 		this.props.onChange && this.props.onChange(newValue);
-	},
+	}
 
 	toggleFormat (formatKey, e) {
 		e.preventDefault();
 		applyFormat(this.codeMirror, formatKey);
-	},
+	}
 
 	renderIcon (icon) {
 		return <span dangerouslySetInnerHTML={{__html: icon}} className="MDEditor_toolbarButton_icon" />;
-	},
+	}
 
 	renderButton (formatKey, label, action) {
 		if (!action) action = this.toggleFormat.bind(this, formatKey);
@@ -106,7 +111,7 @@ var MarkdownEditor = React.createClass({
 				<span className={labelClass}>{label}</span>
 			</button>
 		);
-	},
+	}
 
 	renderToolbar () {
 		return (
@@ -122,7 +127,7 @@ var MarkdownEditor = React.createClass({
 				{/*this.renderButton('link', 'a')*/}
 			</div>
 		);
-	},
+	}
 
 	render () {
 		var editorClassName = classNames('MDEditor_editor', { 'MDEditor_editor--focused': this.state.isFocused });
@@ -130,12 +135,11 @@ var MarkdownEditor = React.createClass({
 			<div className="MDEditor">
 				{this.renderToolbar()}
 				<div className={editorClassName}>
-					<textarea ref="codemirror" name={this.props.path} defaultValue={this.props.value} autoComplete="off" />
+					<textarea ref={ref => this.codemirror = ref} name={this.props.path} defaultValue={this.props.value} autoComplete="off" />
 				</div>
 			</div>
 		);
 	}
+}
 
-});
-
-export default MarkdownEditor;
+export default MarkdownEditor
---
+++ src/index.js
@@ -0,0 +1,8 @@
+// import MDEditor from './MDEditor'
+var MDEditor = require('./MDEditor')
+
+console.log(MDEditor)
+
+export default MDEditor
+// exports['default'] = MDEditor;
+// module.exports = exports['default'];
